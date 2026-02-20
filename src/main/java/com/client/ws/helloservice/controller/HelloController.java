a7d276adf5d1504e5036531d8b136a79253e921e
Emerson Ferreira <eme.ferreira.silva@gmail.com>
2026-02-20 13:45:04 -0300

    first clean commit

---
+++ src/main/java/com/client/ws/helloservice/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.client.ws.helloservice.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from server 🚀";
+    }
+}
